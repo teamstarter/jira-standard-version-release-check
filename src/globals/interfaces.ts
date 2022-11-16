596f1756e8be695e8d20dc84420176fb9966fac7
mvaldes42 <mvaldes@student.42.fr>
2022-11-16 18:40:11 +0100

    feat(options): add help options

--- src/globals/interfaces.ts
+++ src/globals/interfaces.ts
@@ -2,6 +2,7 @@ export interface IOptionsArguments {
   onlyWarnings: boolean;
   table: boolean;
   disableChecks: boolean;
+  help?: boolean;
 }
 
 export interface IRow {
--- src/setUpOptions.ts
+++ src/setUpOptions.ts
@@ -1,3 +1,4 @@
+import { exit } from "process";
 import { parse } from "ts-command-line-args";
 import { IOptionsArguments } from "./globals/interfaces";
 
@@ -5,11 +6,52 @@ export const SOptions = (function () {
   let options: IOptionsArguments;
 
   function setOptions() {
-    const options = parse<IOptionsArguments>({
-      onlyWarnings: { type: Boolean, alias: "w" },
-      table: { type: Boolean, alias: "t" },
-      disableChecks: { type: Boolean, alias: "c" },
-    });
+    try {
+      const options = parse<IOptionsArguments>(
+        {
+          onlyWarnings: {
+            type: Boolean,
+            alias: "w",
+            description: "Only prints warnings",
+          },
+          table: {
+            type: Boolean,
+            alias: "t",
+            description: "Prints as a table format.",
+          },
+          disableChecks: {
+            type: Boolean,
+            alias: "c",
+            description: "Output raw standard-version.",
+          },
+          help: {
+            type: Boolean,
+            optional: true,
+            alias: "h",
+            description: "Prints this usage guide",
+          },
+        },
+        {
+          helpArg: "help",
+          headerContentSections: [
+            {
+              header: "Jira Standard-Version Release Check",
+              content: "Thanks for using Our Awesome Package",
+            },
+          ],
+          footerContentSections: [
+            {
+              header: "Github",
+              content: `https://github.com/teamstarter/jira-standard-version-release-check`,
+            },
+          ],
+        }
+      );
+    } catch {
+      throw new Error(
+        "Wrong arguments, please use --help or -h to see options."
+      );
+    }
     return options;
   }
   return {
