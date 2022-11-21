dbe3292e88f5486acb94af511f9799a60537399d
mvaldes42 <mvaldes@student.42.fr>
2022-11-21 11:24:25 +0100

    fix(code): removed useLocalChangelog function that was used for testing

--- src/checkForRelease.ts
+++ src/checkForRelease.ts
@@ -11,7 +11,6 @@ import { SOptions } from "./setUpOptions";
 module.exports = async function main() {
   getEnvVariables();
   useStandardVersion();
-  // useLocalChangelog();
 };
 
 async function useStandardVersion() {
@@ -43,27 +42,6 @@ async function useStandardVersion() {
     );
     if (options.disableChecks) console.log(line);
     else consoleOutputArray.push(formatLine(lineObj));
-  }
-  printOutput(consoleOutputArray);
-}
-
-async function useLocalChangelog() {
-  if (!process.env.CHANGELOG_FILE)
-    throw new Error("Please set CHANGELOG_FILE variable in .env.");
-  if (!fs.existsSync(process.env.CHANGELOG_FILE))
-    throw new Error(
-      "File referenced by CHANGELOG_FILE variable in .env does not exist."
-    );
-  var user_file = process.env.CHANGELOG_FILE;
-  var r = readline.createInterface({
-    input: fs.createReadStream(user_file),
-  });
-  let consoleOutputArray: (ILine | ILineNoUS | ILineEmpty | undefined)[] = [];
-  for await (const line of r) {
-    const lineObj: ILine | ILineNoUS | ILineEmpty | undefined = await getLine(
-      line
-    );
-    consoleOutputArray.push(formatLine(lineObj));
   }
   printOutput(consoleOutputArray);
 }
