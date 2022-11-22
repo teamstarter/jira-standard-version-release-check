83a5e7124a862fab491f02915cd24325ce681ea2
mvaldes42 <mvaldes@student.42.fr>
2022-11-22 17:41:23 +0100

    add JIRA_PROJECT_KEY to env and search in commits

--- src/getJiraUSFromText.ts
+++ src/getJiraUSFromText.ts
@@ -1,7 +1,10 @@
 export function getJiraUSFromText(line: string) {
-  const found = line.match(/DP.(\d+)/);
+  const regex = new RegExp(`${process.env.JIRA_PROJECT_KEY}.(\\d+)`);
+
+  const found = line.match(regex);
+  debugger
   if (found && found[1]) {
-    return `DP-${found[1]}`;
+    return `${process.env.JIRA_PROJECT_KEY}-${found[1]}`;
   }
   return null;
 }
