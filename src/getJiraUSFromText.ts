6a15ab887d4831be48517af51f88757062854d21
mvaldes42 <mvaldes@student.42.fr>
2023-03-03 18:38:03 +0100

    uppercaseCompare

--- src/getJiraUSFromText.ts
+++ src/getJiraUSFromText.ts
@@ -1,7 +1,7 @@
 export function getJiraUSFromText(line: string) {
-  const regex = new RegExp(`${process.env.JIRA_PROJECT_KEY}.(\\d+)`);
 
-  const found = line.match(regex);
+  const regex = new RegExp(`${process.env.JIRA_PROJECT_KEY?.toUpperCase()}.(\\d+)`);
+  const found = line.toUpperCase().match(regex);
   if (found && found[1]) {
     return `${process.env.JIRA_PROJECT_KEY}-${found[1]}`;
   }
